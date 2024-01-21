cace5df9d8b4255626728321b7947845ebb9cee1
Pranto Mollick <prantocse01@gmail.com>
2024-01-21 17:00:56 +0600

    signin and sign up auth status

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren } from "react";
 
 export default function Providers({ children }: PropsWithChildren) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
