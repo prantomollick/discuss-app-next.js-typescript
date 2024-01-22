154bb4ea15a66b895820c5793a16f204330fa5c1
Pranto Mollick <prantocse01@gmail.com>
2024-01-22 19:03:55 +0600

    receiving from data into the from action functions

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,9 @@
 "use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
   //TODO: revalidate the homepage after creating a topic
+  const name = formData.get("name");
+  const description = formData.get("description");
+
+  console.log(name, description);
 }
