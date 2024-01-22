6351198fb930f2172e2fd38a13273978c46c2690
Pranto Mollick <prantocse01@gmail.com>
2024-01-22 20:50:20 +0600

    setup the useFormState hook with server actions

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,12 +1,31 @@
 "use server";
 import { createTopicSchema } from "@/schema";
 
-export async function createTopic(formData: FormData) {
+export interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   //TODO: revalidate the homepage after creating a topic
+
   const name = formData.get("name");
   const description = formData.get("description");
+
   const validation = createTopicSchema.safeParse({ name, description });
 
-  if (!validation.success) console.log(validation.error.flatten().fieldErrors);
-  // console.log(name, description);
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors
+    };
+  }
+
+  return {
+    errors: {}
+  };
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Button,
   Input,
@@ -9,15 +10,20 @@ import {
 import React from "react";
 
 import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {}
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a topic</h3>
             <Input
